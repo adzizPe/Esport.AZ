0ba9b38bcc36497272ca2b395ed699e0888528d1
adzizPe <adzanaziz2@gmail.com>
2024-12-27 16:59:45 +0700

    +

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+  });
+}
+
+
+
+/**
+ * search toggle
+ */
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    searchBox.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+// time current
+function updateTime() {
+  const now = new Date();
+  const options = { 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric', 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      second: '2-digit', 
+      hour12: false 
+  };
+  const formattedTime = now.toLocaleString('id-ID', options); // Menggunakan format Indonesia
+  document.getElementById('current-time').innerText = formattedTime;
+}
+
+// Memperbarui waktu setiap detik
+setInterval(updateTime, 1000);
+
+// Memanggil fungsi pertama kali untuk menampilkan waktu segera
+updateTime(); 
+// changing the favicon
+const link = document.querySelector("link[rel= 'icon']");
+let i = 0;
+setInterval(() => link.href =`${++i % 3}.png` , 500);
